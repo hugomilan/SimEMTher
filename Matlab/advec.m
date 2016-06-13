f0808366e48d44702134c1e401c7e8dc09825731
Hugo Fernando Maia Milan <hugofernando@gmail.com>
2016-06-13 01:55:22 -0400

    Update advec.m

--- Matlab/advec.m
+++ Matlab/advec.m
@@ -1,3 +1,5 @@
+%Copyright &copy; 2010 to 2016. Hugo Fernando Maia Milan (hugofernando@gmail.com).
+
 function [ADSB, ADNO, ADCON] = advec(Zterx, Ztery, Zter, Z2c, ...
     Gter, Tempin, emis, diaanimal, Kter, nnosx, nnosy)
 global dimenmalha ent dl fluxosext
@@ -17,8 +19,8 @@ if fluxosext(1,1) ~= 0
                     ADNO(vff,1) = (a-(ceil(a/nnosx)-1)*nnosx);
                     ADCON(vff,3) = Tempin(fluxosext(qfl,2)); %Temperatura
                     ADCON(vff,4) = fluxosext(qfl,3);
-                    ADCON(vff,1) = convadvuni(Zter, Gter, Kter, diaanimal, ADCON(vff,3), fluxosext(qfl,2), fluxosext(qfl,3)); %convecção
-                    ADCON(vff,2) = radadvuni(Zter, Gter, dl, emis, fluxosext(qfl,2), fluxosext(qfl,3)); %radiação
+                    ADCON(vff,1) = convadvuni(Zter, Gter, Kter, diaanimal, ADCON(vff,3), fluxosext(qfl,2), fluxosext(qfl,3)); %convecÃ§Ã£o
+                    ADCON(vff,2) = radadvuni(Zter, Gter, dl, emis, fluxosext(qfl,2), fluxosext(qfl,3)); %radiaÃ§Ã£o
                     vff = vff + 1;
                 end
             end
@@ -35,8 +37,8 @@ if fluxosext(1,1) ~= 0
                     ADSB(vff,:) = [ceil(a/nnosy) ceil(b/nnosx)];
                     ADNO(vff,:) = [(a-(ceil(a/nnosy)-1)*nnosy) (b-(ceil(b/nnosx)-1)*nnosx)];
                     ADCON(vff,3) = Tempin(fluxosext(qfl,2)); %Temperatura
-                    ADCON(vff,1) = convadvbi(Zterx, Ztery, Z2c, Kter, diaanimal, fluxosext(qfl,2), fluxosext(qfl,3)); %convecção
-                    ADCON(vff,2) = radadvbi(Zterx, Ztery, Z2c, dl, emis, 1, fluxosext(qfl,2), fluxosext(qfl,3)); %radiação
+                    ADCON(vff,1) = convadvbi(Zterx, Ztery, Z2c, Kter, diaanimal, fluxosext(qfl,2), fluxosext(qfl,3)); %convecÃ§Ã£o
+                    ADCON(vff,2) = radadvbi(Zterx, Ztery, Z2c, dl, emis, 1, fluxosext(qfl,2), fluxosext(qfl,3)); %radiaÃ§Ã£o
                     vff = vff + 1;
                 end
                 end
